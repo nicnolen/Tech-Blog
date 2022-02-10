9a16c37504b75107350dc360dda6841828b7b0cc
Nicholas Nolen <nicnolen@ymail.com>
2022-02-10 12:45:02 -0600

    set up database in MySQL

---
+++ server.js
@@ -0,0 +1,9 @@
+/* NODE.JS MODULES */
+// Import the Path module to work with directories and file paths
+const path = require('path');
+
+/* DEPENDENCIES */
+// Import express.js
+const express = require('express');
+
+
