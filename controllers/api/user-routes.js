8198ab427f06ca7eca9fd6f1df653d9726308286
Nicholas Nolen <nicnolen@ymail.com>
2022-02-11 09:42:58 -0600

    added a get route to find all the users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,18 @@
-/* ROUTES FOR THE USER MODEL */
+/* ROUTES FOR THE USER MODEL (/api/users) */
 // Import Express.js Router() and the User model
 const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users (all users)
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  // access the User model and find all users using .findAll()
+  User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+});
 
 // GET /api/users/id (search user by id)
 router.get('/:id', (req, res) => {});
@@ -16,4 +24,4 @@ router.post('/', (req, res) => {});
 router.put('/:id', (req, res) => {});
 
 // DELETE /api/users/1 (delete a user by id)
-router.delete('/:id', (req, res) => {})
+router.delete('/:id', (req, res) => {});
