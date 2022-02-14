7bd59662a9ab8c9b630c0e202795a3ea6772ac49
Nicholas Nolen <nicnolen@ymail.com>
2022-02-14 11:54:58 -0600

    wrote failing tests to shorten the URL

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -17,3 +17,14 @@ test('format_plural() returns a pluralized word', () => {
   expect(word1).toBe('tiger');
   expect(word2).toBe('lions');
 });
+
+// Test to shorten URL
+test('format_url() returns a simplified url string', () => {
+  const url1 = format_url('http://test.com/page/1');
+  const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+  const url3 = format_url('https://www.google.com?q=hello');
+
+  expect(url1).toBe('test.com');
+  expect(url2).toBe('coolstuff.com');
+  expect(url3).toBe('google.com');
+});
