a26e4c273475b29a4b6f5f8fa5ebc02789a5f3e0
Nicholas Nolen <nicnolen@ymail.com>
2022-02-14 09:31:23 -0600

    replaced hardcoded code with a Post.findOne() query

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -52,18 +52,50 @@ router.get('/login', (req, res) => {
 
 // GET post by id (single post)
 router.get('/post/:id', (req, res) => {
-  const post = {
-    id: 1,
-    post_url: 'https://handlebarsjs.com/guide/',
-    title: 'Handlebars Docs',
-    created_at: new Date(),
-    comments: [{}, {}],
-    user: {
-      username: 'test_user',
+  Post.findOne({
+    where: {
+      id: req.params.id,
     },
-  };
+    attrbutes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      { model: User, attributes: ['username'] },
+    ],
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
-  res.render('single-post', { post });
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { post });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 });
 
 // Export the module
