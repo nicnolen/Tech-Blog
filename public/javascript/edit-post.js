e8070f617332450b7b62c38a5046485969857625
Nicholas Nolen <nicnolen@ymail.com>
2022-02-17 08:29:01 -0600

    updated README.md

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -6,13 +6,16 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  const content = document.querySelector('input[name="content"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
+      post_id: id,
       title,
+      content,
     }),
     headers: {
       'Content-Type': 'application/json',
